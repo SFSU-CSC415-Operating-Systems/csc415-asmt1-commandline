740db09a76dd07d6a4972cd356c8f83c260bcfca
student <mkim797@yahoo.com>
2022-09-01 16:57:54 -0700

    code complete with commenting

--- Kim_Mark_HW1_main.c
+++ Kim_Mark_HW1_main.c
@@ -7,10 +7,19 @@
 *
 * File: Kim_Mark_HW1_main.c 
 *
-* Description:
+* Description: This project program first displays the number of
+* arguments provided to this executable. In subsequent
+* lines it will list the argument number with the contentd of the
+* argument alongside it.  All output is sent to the console.
 *
 **************************************************************/
 #include <stdio.h>
-int main () {
-	printf ("Hello World\n");
+int main ( int argc, char *argv[] ) {
+	// Print number of arguments (stored in argc)
+	printf("\nThere were %d arguments on the command line.\n", argc);
+	// Loop through *argv array (index 0 is the name of the executable,
+	// and subsequent indices are the arguments provided)
+	for (int i=0; i < argc; i++) {
+		printf("Argument %02d:\t%s\n", i, argv[i]);
+	}
 }
